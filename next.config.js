81fce93fcfddb70f6147c47d120b529379ed3252
Lukas Nilsson <lukaspnilsson@gmail.com>
2025-05-05 00:38:23 +1000

    fix: remove invalid experimental.serverActions flag

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      serverActions: true, // optional, if you're using it
-    },
   };
   
   module.exports = nextConfig;
